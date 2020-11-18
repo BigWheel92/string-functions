e7795c289eeaf2e644d1864da2e62ef7137080dc
Saad Farooq <saad92.sf@gmail.com>
2020-11-18 23:24:48 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,28 @@ int main()
 	char subString[10] = "Comp";
 	cout << myString << endl;
 	findAndReplace(myString, subString, "Computer");
-	cout << myString;
+	cout << myString << endl;
+	
+	doubleToCharArr(-29.999, subString, 2, true);
+	cout << subString << endl;
+
+	doubleToCharArr(-29.999, subString, 2, false);
+	cout << subString << endl;
+
+	doubleToCharArr(-9.99, subString, 1, true);
+	cout << subString << endl;
+
+	doubleToCharArr(-0.999, subString, 2, true);
+	cout << subString << endl;
+
+	doubleToCharArr(-0.999, subString, 0, true);
+	cout << subString << endl;
+
+	doubleToCharArr(-0.999, subString, 0, false);
+	cout << subString << endl;
+
+	doubleToCharArr(-0.099, subString, 1, false);
+	cout << subString << endl;
+
 	return 0;
 }
